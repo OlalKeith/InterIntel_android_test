ae849a06c9c72abd6eef869f82152eab70bae337
Olal Keith <jkeneth.jk@gmail.com>
2019-02-08 17:31:09 +0300

    Setting up SQLiteOpenhelper

--- app2/src/main/java/com/example/app2/DatabaseHelper.java
+++ app2/src/main/java/com/example/app2/DatabaseHelper.java
@@ -1,6 +1,23 @@
 package com.example.app2;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String TABLE_NAME = "users_table";
+    public static final int DATABASE_VERSION = 1;
+    public DatabaseHelper(Context context) {
+        super(context, TABLE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
